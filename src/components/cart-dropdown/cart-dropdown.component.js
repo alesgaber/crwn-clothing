d2d5471fe1ab4e22c58409845d9d74f20a1b3eb4
alesgaber <ales.gaber@gmail.com>
2021-01-29 18:19:48 +0100

    updated cart data with new data, update cart utils to group items

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -5,9 +5,8 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => (
   <div className="cart-dropdown">
-    <div className="cart-items">
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
+    <div className="cart-items" />
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
