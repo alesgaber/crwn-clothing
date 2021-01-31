193d0472e6120a7c57319e5642d4ad16e6792436
alesgaber <ales.gaber@gmail.com>
2021-01-31 13:14:02 +0100

    replacing selectors with memoized selectors

--- src/components/card-icon/card-icon.component.js
+++ src/components/card-icon/card-icon.component.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import './card-icon.styles.scss';
 
-const CardIcon = ({ onToggleHidden }) => (
+const CardIcon = ({ onToggleHidden, itemCount }) => (
   <div className="cart-icon">
     <ShoppingIcon onClick={onToggleHidden} />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -15,4 +18,8 @@ const mapDispatchToProps = (dispatch) => ({
   onToggleHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CardIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardIcon);
