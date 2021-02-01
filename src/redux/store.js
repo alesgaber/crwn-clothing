7b3c87e717c352ea5a222e06b37e76dad9812f77
alesgaber <ales.gaber@gmail.com>
2021-02-01 14:17:26 +0100

    moved shop data into reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
+import { persistStore } from 'redux-persist';
 import rootReducers from './root.reducer';
 
 // midlewares
 const midlewares = [logger];
 
-const store = createStore(rootReducers, applyMiddleware(...midlewares));
+export const store = createStore(rootReducers, applyMiddleware(...midlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+// export { store, persistor };
