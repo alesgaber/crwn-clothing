a68d1f220f0fe077381f5234737a496eac16958f
alesgaber <ales.gaber@gmail.com>
2021-03-08 09:15:36 +0100

    implementing  PWA

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
+import * as serviceWorker from './service-worker';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -21,6 +23,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const enforce = require('express-sslify');
+
 
 const compression = require("compression")
+
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -15,6 +18,8 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
+
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
@@ -28,6 +33,10 @@ app.listen(port, error => {
   if (error) throw error;
   console.log("Server running on port " + port);
 });
+
+app.get('/service-worker.js', (req, res)=> {
+  req.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+})
 
 app.post("/payment", (req, res) => {
   const body = {
